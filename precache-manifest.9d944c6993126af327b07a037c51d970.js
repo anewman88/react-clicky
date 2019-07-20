59e3f805333298ad182368bfefa0b0891fb6e8fe
Ann Newman <anewman1822@gmail.com>
2019-07-19 21:44:41 -0500

    Updates

---
+++ precache-manifest.9d944c6993126af327b07a037c51d970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d126b5ebb5af4d5bfc77634d74815a4",
+    "url": "/react-clicky/index.html"
+  },
+  {
+    "revision": "7786e59f11accd60c4b2",
+    "url": "/react-clicky/static/css/main.78e9605b.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-clicky/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "7786e59f11accd60c4b2",
+    "url": "/react-clicky/static/js/main.d0ceb4b2.chunk.js"
+  },
+  {
+    "revision": "4f60053edc30175445e5",
+    "url": "/react-clicky/static/js/runtime~main.e19a1b38.js"
+  },
+  {
+    "revision": "714894d8ccb0b658b2291e1b8f862b08",
+    "url": "/react-clicky/static/media/background.714894d8.jpeg"
+  },
+  {
+    "revision": "58f517ffb736e06192571aba42c01853",
+    "url": "/react-clicky/static/media/granite.58f517ff.jpg"
+  }
+]);
