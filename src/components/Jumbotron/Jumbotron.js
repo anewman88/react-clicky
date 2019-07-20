5b30b713e856acd572d5a610b74ab515337ae17d
Ann Newman <anewman1822@gmail.com>
2019-07-19 22:29:22 -0500

    cleaned up styling

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,9 +5,9 @@ import "./Jumbotron.css";
 const Jumbotron = () => (
 	<div>
     <div className="jumbotron">
-      <h1>Crysal Clicky Game!</h1>
+      <h1>Crystal Clicky Game!</h1>
       <h2>Click on any crystal to earn a point, but don't click on the same one more than once.</h2> 
-      <h2>Click all 12 crystals, without repeating, and you win.</h2>
+      <h2>Click all 15 crystals, without repeating, and you win.</h2>
     </div>
 	</div>
 );
